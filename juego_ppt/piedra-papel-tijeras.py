6781e8727d69b01fc4c412e485e546e3712b25b9
lizfore <andresrivera@192.168.0.13>
2026-05-14 06:38:54 -0500

    agregamos el juego piedra papel tjeras

---
+++ juego_ppt/piedra-papel-tijeras.py
@@ -0,0 +1,10 @@
+jugador1 = input("Jugador 1, elige piedra, papel o tijeras: ")
+jugador2 = input("Jugador 2, elige piedra, papel o tijeras: ")  
+
+if jugador1 == jugador2:    
+    print("Empate!")
+elif (jugador1 == "piedra" and jugador2 == "tijeras") or (jugador1 == "papel" and jugador2 == "piedra") or (jugador1 == "tijeras" and jugador2 == "papel"):
+    print("Jugador 1 gana!")
+else:    
+    print("Jugador 2 gana!")
+
